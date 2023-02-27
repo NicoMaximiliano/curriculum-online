e3b63da9d4373f7a43ad40b345b91a4a1266623c
NicoMaximiliano <nicolas.orellano25@outlook.com.ar>
2023-02-27 05:36:01 -0300

    Actualizacion final del proyecto

--- js/curriculum.js
+++ js/curriculum.js
@@ -1,37 +1,50 @@
 
-var descripcion = localStorage.getItem('descripcion');
+var botonImprimir = document.querySelector("#boton-imprimir");
+var botonVolver = document.querySelector("#boton-volver");
+
 var nombre = localStorage.getItem('nombre');
 var apellido = localStorage.getItem('apellido');
 var edad = localStorage.getItem('edad');
+var estado_civil = localStorage.getItem('estado civil');
+var fotoPerfil = localStorage.getItem('foto');
+var descripcion = localStorage.getItem('descripcion');
 var telefono = localStorage.getItem('telefono');
 var email = localStorage.getItem('email');
+var linkedin = localStorage.getItem('linkedin');
 
-
-document.querySelector('.titulo-nombre').innerHTML = nombre + " " + apellido;
-document.querySelector('.descripcion').innerHTML = descripcion;
-document.querySelector('.nombre').innerHTML = nombre + " " + apellido;
-document.querySelector('.edad').innerHTML = edad;
-document.querySelector('.telefono').innerHTML = telefono;
-document.querySelector('.email').innerHTML = email;
-
+cargarDatosPersonales();
 obtenerHabilidades();
-obtenerEstudios();
+obtenerEducacion();
 obtenerCertificaciones();
 
+
+function cargarDatosPersonales() {
+    document.querySelector('.titulo-nombre').innerHTML = nombre + " " + apellido;
+    document.querySelector('#edad').innerHTML = edad;
+    document.querySelector('#estado-civil').innerHTML = estado_civil;
+    document.querySelector(".foto-perfil").src = fotoPerfil;
+    document.querySelector('.descripcion').innerHTML = descripcion;
+    document.querySelector('#telefono').innerHTML = telefono;
+    document.querySelector('#email').innerHTML = email;
+    document.querySelector('#linkedin').innerHTML = linkedin;
+}
+
 function obtenerHabilidades() {
-    var cantidad = localStorage.getItem('cantidadH');
+    let cantidad = localStorage.getItem('cantidadH');
 
     for(let i=0;i<cantidad;i++){
-        var habilidad = localStorage.getItem('habilidad #'+i);
-        agregarHabilidadElementoLi(habilidad);
+        let habilidad = localStorage.getItem('habilidad #'+i);
+        agregarHabilidadEnElementoLi(habilidad);
     }
 }
 
-function agregarHabilidadElementoLi(habilidad) {
-    var listaH = document.querySelector('.lista-habilidades');
+function agregarHabilidadEnElementoLi(habilidad) {
+    let listaH = document.querySelector('.lista-habilidades');
 
-    var li = document.createElement('li');
-    var p = document.createElement('p');
+    let li = document.createElement('li');
+    let p = document.createElement('p');
+
+    p.classList.add("form-p");
 
     listaH.appendChild(li);
     li.appendChild(p);
@@ -39,42 +52,45 @@ function agregarHabilidadElementoLi(habilidad) {
     p.textContent = habilidad;
 }
 
-function obtenerEstudios() {
-    var cantidad = localStorage.getItem('cantidadE');
+function obtenerEducacion() {
+    let cantidad = localStorage.getItem('cantidadE');
 
     for(let i=0;i<cantidad;i++){
-        var estudio = localStorage.getItem('estudio #'+i);
-        agregarEstudioElementoLi(estudio);
+        let educacion = localStorage.getItem('educacion #'+i);
+        agregarEducacionEnElementoLi(educacion);
     }
 }
 
-function agregarEstudioElementoLi(estudio) {
-    var listaE = document.querySelector('.lista-estudios');
+function agregarEducacionEnElementoLi(educacion) {
+    let listaI = document.querySelector('.listado-educacion');
 
-    var li = document.createElement('li');
-    var p = document.createElement('p');
+    let li = document.createElement('li');
+    let p = document.createElement('p');
 
-    listaE.appendChild(li);
+    p.classList.add("form-p");
+
+    listaI.appendChild(li);
     li.appendChild(p);
 
-    p.textContent = estudio;
+    p.textContent = educacion;
 }
 
 function obtenerCertificaciones() {
-    var cantidad = localStorage.getItem('cantidadC');
+    let cantidad = localStorage.getItem('cantidadC');
 
     for(let i=0;i<cantidad;i++){
-        var certificacion = localStorage.getItem('certificacion #'+i);
-        //document.querySelector('#parrafo'+i).innerHTML = habilidad;
-        agregarCertificacionElementoLi(certificacion);
+        let certificacion = localStorage.getItem('certificacion #'+i);
+        agregarCertificacionEnElementoLi(certificacion);
     }
 }
 
-function agregarCertificacionElementoLi(certificacion) {
-    var listaC = document.querySelector('.lista-certificaciones');
+function agregarCertificacionEnElementoLi(certificacion) {
+    let listaC = document.querySelector('.lista-certificaciones');
 
-    var li = document.createElement('li');
-    var p = document.createElement('p');
+    let li = document.createElement('li');
+    let p = document.createElement('p');
+
+    p.classList.add("form-p");
 
     listaC.appendChild(li);
     li.appendChild(p);
@@ -82,5 +98,89 @@ function agregarCertificacionElementoLi(certificacion) {
     p.textContent = certificacion;
 }
 
+botonImprimir.addEventListener("click", function (evento) {
+   evento.preventDefault();
 
-localStorage.clear();
+   let header = document.querySelector(".cabecera-curriculum");
+   let form = document.querySelector(".form-curriculum");
+
+   let estilo = 'body{\n' +
+       '    font-family: \'Quicksand\', sans-serif;\n' +
+       '    background-color: #d6eaff;\n' +
+       '}\n' +
+       'main{\n' +
+       '    margin-bottom: 20px;\n' +
+       '}\n' +
+       'h1,h2{\n' +
+       '    font-weight: bold;\n' +
+       '}\n' +
+       'h2{\n' +
+       '    padding-top: 30px;\n' +
+       '}\n' +
+       'form{\n' +
+       '    background-color: #fff;\n' +
+       '    border-radius: 10px;\n' +
+       '    border: 2px solid; \n' +
+       '}\n' +
+       '.container{\n' +
+       '    display: flex;\n' +
+       '    text-align: center;\n' +
+       '    padding: 0;\n' +
+       '}\n' +
+       '.foto-perfil{\n' +
+       '    width: 300px;\n' +
+       '    height: 300px;\n' +
+       '    border-radius: 10px;\n' +
+       '}\n' +
+       '.container-perfil{\n' +
+       '    display: flex;\n' +
+       '    flex-direction: column;\n' +
+       '    margin-left: 10px;\n' +
+       '}\n' +
+       '.form-p{\n' +
+       '    font-weight: bold;\n' +
+       '    border-bottom: 1px solid #dee2e6;\n' +
+       '}\n' +
+       '#titulo-perfil-profesional, #titulo-info-personal, #titulo-datos-contacto,\n' +
+       '#titulo-habilidades, #titulo-educacion, #titulo-certificaciones{\n' +
+       '    border-bottom: 3px solid #0d6efd;\n' +
+       '}\n' +
+       '.form-label{\n' +
+       '    font-weight: bold;\n' +
+       '}\n' +
+       'ul{\n' +
+       '    padding-left: 0;\n' +
+       '}\n' +
+       'li{\n' +
+       '    list-style: none;\n' +
+       '}\n' +
+       '.certificaciones-curriculum{\n' +
+       '    padding-bottom: 30px;\n' +
+       '}\n';
+
+   imprimirCurriculumEnPDF(form, header, estilo);
+});
+
+function imprimirCurriculumEnPDF(form, header, estilo) {
+    let ventana = window.open('', 'PRINT', 'height=400,width=600');
+
+    ventana.document.write('<html><head><title>' + document.title + '</title>');
+    ventana.document.write('<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>');
+    ventana.document.write('<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;500;700&display=swap" rel="stylesheet">');
+    ventana.document.write('<style>');
+    ventana.document.write(estilo);
+    ventana.document.write('</style>');
+    ventana.document.write('</head><body >');
+    ventana.document.write(header.innerHTML);
+    ventana.document.write(form.innerHTML);
+    ventana.document.write('</body></html>');
+    ventana.document.close();
+
+    ventana.focus();
+    ventana.print();
+}
+
+botonVolver.addEventListener("click",function (evento) {
+    localStorage.clear();
+});
+
--- js/form.js
+++ js/form.js
@@ -1,147 +1,292 @@
 window.localStorage;
 
-const arrayHabilidades = new Array();
-const arrayEstudios = new Array();
-const arrayCertificaciones = new Array();
+const arregloHabilidades = new Array();
+const arregloEducacion = new Array();
+const arregloCertificaciones = new Array();
 
+var form = document.querySelector(".form-curriculum");
+
+var checkBoxesEstadoCivil = document.querySelectorAll(".form-check-input");
+var estadoCivil;
+
+var fotoPerfil = document.querySelector("#foto");
+var srcImagen;
+
+var descripcionPerfil = document.querySelector("#descripcion");
+var contadorCaracter = 0;
 
 var botonAgregarHabilidad = document.querySelector("#agregar-habilidad");
-var checkBoxesEstudio = document.querySelectorAll(".form-check-input");
+var botonAgregarEducacion = document.querySelector("#agregar-educacion");
 var botonAgregarCertificacion = document.querySelector("#agregar-certificacion");
-var botonGenerarCurriculum = document.querySelector("#generar-curriculum");
+
+var botonGenerarCurriculum = document.querySelector("#boton-generar-curriculum");
+
+
+
+/*
+var entradas = document.querySelectorAll("input");
+
+var areaTexto = document.querySelector("textarea");
+
+areaTexto.addEventListener("blur", function (evento) {
+   var elemento = evento.target;
+
+   if(elemento.validity.valueMissing){
+      areaTexto.parentElement.querySelector(".mensaje-error").innerHTML = "Este campo no puede estar vacio";
+      areaTexto.style.borderColor = "red";
+      console.log(elemento.name);
+   }
+   else{
+      areaTexto.parentElement.querySelector(".mensaje-error").innerHTML = " ";
+      areaTexto.style.borderColor = "#dee2e6";
+      console.log(elemento.value);
+   }
+});
+
+
+entradas.forEach((entrada) => {
+
+   entrada.addEventListener("blur", function (evento) {
+      var elemento = evento.target;
+
+      if(elemento.validity.valueMissing){
+         entrada.parentElement.querySelector(".mensaje-error").innerHTML = "Este campo no puede estar vacio";
+         entrada.style.borderColor = "red";
+         console.log(elemento.name);
+      }
+      else{
+         entrada.parentElement.querySelector(".mensaje-error").innerHTML = " ";
+         entrada.style.borderColor = "#dee2e6";
+         console.log(elemento.value);
+      }
+   });
+});
+ */
+
+checkBoxesEstadoCivil.forEach((checkBox) => {
+
+   checkBox.addEventListener("click", function (evento) {
+      let valor = checkBox.value;
+
+      if(checkBox.checked){
+         estadoCivil = valor;
+      }
+   });
+});
+
+
+fotoPerfil.addEventListener("change",function (evento) {
+   let archivos = evento.target.files;
+   let archivo = archivos[0];
+
+   const reader = new FileReader();
+   reader.readAsDataURL(archivo);
+   reader.onload = (e) => {
+      e.preventDefault();
+      srcImagen = e.target.result;
+      console.log(srcImagen);
+   };
+});
+
+
+descripcionPerfil.addEventListener("keydown", function (evento) {
+
+   let cantidadCaracteres = document.querySelector(".numero-caracteres");
+
+   /*
+   if (evento.ctrlKey){
+      if (evento.keyCode === 86) {
+         console.log("ctrl v");
+      }
+   }
+    */
+
+   if((evento.keyCode >= 65 && evento.keyCode <= 90) || (evento.keyCode === 32)){
+      contadorCaracter++;
+
+      if(contadorCaracter <= 600){
+         cantidadCaracteres.innerHTML = contadorCaracter;
+      }
+   }
+
+   //console.log(evento.key);
+   //console.log(evento.keyCode);
+   //console.log(contador);
+
+});
 
 
 botonAgregarHabilidad.addEventListener("click",function (evento) {
    evento.preventDefault();
 
-   var habilidad = document.getElementById("habilidad").value;
-   agregarHabilidades(habilidad);
+   let habilidad = document.getElementById("habilidad").value;
 
-   //var habilidades = getHabilidades();
+   agregarElementosAlArreglo(habilidad,"h");
 
-   //console.log(habilidades);
-   //alert("Habilidad agregada");
+   manipularListadoEnHTML(habilidad,"h");
+
 });
 
-function agregarHabilidades(habilidad) {
-   arrayHabilidades.push(habilidad);
-}
 
-function getHabilidades(){
-   return arrayHabilidades;
+function obtenerHabilidades(){
+   return arregloHabilidades;
 }
 
 
+botonAgregarEducacion.addEventListener("click",function (evento) {
+   evento.preventDefault();
 
-checkBoxesEstudio.forEach((checkBox) => {
+   let educacion = document.getElementById("educacion").value;
 
-   checkBox.addEventListener("click", function (evento) {
-      var valor = checkBox.value;
+   agregarElementosAlArreglo(educacion,"e");
 
-      if(checkBox.checked){
-         //console.log(valor);
-         agregarEstudio(valor);
-      }
-      else{
-         //console.log("Desmarcado " + valor);
-         eliminarEstudio(valor);
-      }
-   });
+   manipularListadoEnHTML(educacion,"e");
+
 });
 
-function agregarEstudio(estudio) {
-   arrayEstudios.push(estudio);
-   //console.log(getEstudios());
-}
 
-function eliminarEstudio(estudio) {
-   var i = arrayEstudios.findIndex(e => e === estudio);
-   arrayEstudios.splice(i,1);
-   //console.log(getEstudios());
+function obtenerEducacion(){
+   return arregloEducacion;
 }
-
-function getEstudios() {
-   return arrayEstudios;
-}
-
 
 
 botonAgregarCertificacion.addEventListener("click",function (evento) {
    evento.preventDefault();
 
-   var certificacion = document.getElementById("certificacion").value;
+   let certificacion = document.getElementById("certificacion").value;
 
-   agregarCertificaciones(certificacion);
+   agregarElementosAlArreglo(certificacion,"c");
 
-   //var certificaciones = getCertificaciones();
-   //console.log(certificaciones);
-   //alert("Certificacion agregada");
+   manipularListadoEnHTML(certificacion,"c");
 });
 
-function agregarCertificaciones(certificacion) {
-   arrayCertificaciones.push(certificacion);
+
+function obtenerCertificaciones(){
+   return arregloCertificaciones;
 }
 
-function getCertificaciones(){
-   return arrayCertificaciones;
+function agregarElementosAlArreglo(valor,tipo) {
+
+   if(tipo === 'h'){
+      arregloHabilidades.push(valor);
+   }
+   else if(tipo === 'e'){
+      arregloEducacion.push(valor);
+   }
+   else if (tipo === 'c'){
+      arregloCertificaciones.push(valor);
+   }
 }
 
 
+function manipularListadoEnHTML(valor,tipo) {
+   let arregloLi;
+   let lista;
+
+   let li = document.createElement('li');
+   let p = document.createElement('p');
+   let boton = document.createElement('button');
+
+   if(tipo === 'h'){
+      lista = document.querySelector('.listado-habilidades');
+      arregloLi = obtenerHabilidades();
+   }
+   else if(tipo === 'e'){
+      lista = document.querySelector('.listado-educacion');
+      arregloLi = obtenerEducacion();
+   }
+   else if (tipo === 'c'){
+      lista = document.querySelector('.listado-certificaciones');
+      arregloLi = obtenerCertificaciones();
+   }
+
+
+   li.classList.add('elemento-li');
+
+   p.textContent = valor;
+
+   boton.type = "button";
+   boton.classList.add('btn-close');
+
+   li.appendChild(p);
+   li.appendChild(boton);
+   lista.appendChild(li);
+
+
+   //ELIMINAR ELEMENTO AL PRESIONAR EL BOTON X
+   boton.addEventListener("click", function (evento) {
+      evento.preventDefault();
+
+      lista.removeChild(li);
+
+      for(let i=0;i<arregloLi.length;i++){
+         if(valor === arregloLi[i]){
+            arregloLi.splice(i,1);
+         }
+      }
+   });
+}
 
 
 botonGenerarCurriculum.addEventListener("click", function (evento) {
    evento.preventDefault();
 
-   var form = document.querySelector("#form-curriculum");
-   var usuario = capturarDatosUsuario(form);
-
-   guardarDatosEnLocalStorage(usuario);
+   let usuario = capturarDatosUsuario();
 
    cambiarBoton();
 
-   form.reset();
+   guardarDatosEnLocalStorage(usuario);
 
-   console.log(usuario);
+   form.reset();
 });
 
-function cambiarBoton() {
-   var boton = document.querySelector('#generar-curriculum');
-   boton.style.backgroundColor = 'green';
-   boton.textContent = 'Curriculum generado!!!'
-}
 
-function capturarDatosUsuario(form) {
+function capturarDatosUsuario() {
 
-   var usuario = {
+   let usuario = {
       nombre : form.nombre.value,
       apellido : form.apellido.value,
       edad : form.edad.value,
+      estado_civil: estadoCivil,
+      foto : srcImagen,
       descripcion : form.descripcion.value,
       telefono : form.telefono.value,
       email : form.email.value,
-      habilidades : getHabilidades(),
-      estudios : getEstudios(),
-      certificaciones : getCertificaciones()
+      linkedin : form.linkedin.value,
+      habilidades : obtenerHabilidades(),
+      educacion : obtenerEducacion(),
+      certificaciones : obtenerCertificaciones()
    }
 
    return usuario;
 }
 
+function cambiarBoton() {
+   let boton = document.querySelector('#boton-generar-curriculum');
+   boton.style.backgroundColor = 'green';
+   boton.textContent = 'Curriculum generado!!!';
+}
+
+
 function guardarDatosEnLocalStorage(usuario) {
    localStorage.setItem('nombre',usuario.nombre);
    localStorage.setItem('apellido',usuario.apellido);
    localStorage.setItem('edad',usuario.edad);
+   localStorage.setItem('estado civil',usuario.estado_civil);
+   localStorage.setItem('foto',usuario.foto);
    localStorage.setItem('descripcion',usuario.descripcion);
    localStorage.setItem('telefono',usuario.telefono);
    localStorage.setItem('email',usuario.email);
+   localStorage.setItem('linkedin',usuario.linkedin);
+
 
    guardarHabilidadesEnLocalStorage(usuario);
-   guardarEstudiosEnLocalStorage(usuario);
+   guardarEducacionEnLocalStorage(usuario);
    guardarCertificacionesEnLocalStorage(usuario);
 }
 
 function guardarHabilidadesEnLocalStorage(usuario){
-   var cantidadHabilidades = usuario.habilidades.length;
+   let cantidadHabilidades = usuario.habilidades.length;
 
    for (let i=0;i<cantidadHabilidades;i++){
       localStorage.setItem('habilidad #'+i, usuario.habilidades[i])
@@ -149,21 +294,17 @@ function guardarHabilidadesEnLocalStorage(usuario){
    localStorage.setItem('cantidadH',cantidadHabilidades);
 }
 
+function guardarEducacionEnLocalStorage(usuario){
+   let cantidadEducacion = usuario.educacion.length;
 
-function guardarEstudiosEnLocalStorage(usuario){
-   var cantidadEstudios = usuario.estudios.length;
-
-
-   for (let i=0;i<cantidadEstudios;i++){
-      localStorage.setItem('estudio #'+i, usuario.estudios[i])
+   for (let i=0;i<cantidadEducacion;i++){
+      localStorage.setItem('educacion #'+i, usuario.educacion[i])
    }
-   localStorage.setItem('cantidadE',cantidadEstudios);
+   localStorage.setItem('cantidadE',cantidadEducacion);
 }
 
-
 function guardarCertificacionesEnLocalStorage(usuario){
-   var cantidadCertificaciones = usuario.certificaciones.length;
-
+   let cantidadCertificaciones = usuario.certificaciones.length;
 
    for (let i=0;i<cantidadCertificaciones;i++){
       localStorage.setItem('certificacion #'+i, usuario.certificaciones[i])
